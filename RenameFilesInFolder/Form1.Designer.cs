ca6378454d410b8bafd43d481bda9b3d742f6a21
Atlas <edgarjscg02@gmail.com>
2020-10-31 11:51:10 -0400

    Project created

---
+++ RenameFilesInFolder/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace RenameFilesInFolder
+{
+    partial class frmRenameFiles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {            
+            // 
+            // frmRenameFiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtProcess);
+            this.Controls.Add(this.txtBeginIn);
+            this.Controls.Add(this.lblBeginIn);
+            this.Controls.Add(this.lblPrefix);
+            this.Controls.Add(this.txtPrefix);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "frmRenameFiles";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rename Files";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
